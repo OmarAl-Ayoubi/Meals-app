b1dd4015299c5e676fdcfbfab38856e36f758d30
Omar Al-Ayoubi <omar.ayoubi@telepaty.org>
2021-04-12 13:22:33 +0300

    redux first lec

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { LogBox } from "react-native";
 import * as Font from "expo-font";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import MealsNavigator from "./navigation/MealsNavigator";
 import { enableScreens } from "react-native-screens";
+import mealsReducer from "./store/reducers/meals";
+
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
+
 const fetchFonts = () => {
   Font.useFonts({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
